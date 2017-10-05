fc1fb025384724c7b3e030124d2840c73db48662
jackeer@hotmail.com <jackeer@hotmail.com>
2017-10-05 21:28:38 +0800

    First Commit
    Signed-off-by: jackeer@hotmail.com <jackeer@hotmail.com>

---
+++ src/com/sam/adbtool/ADB_Main.java
@@ -0,0 +1,369 @@
+package com.sam.adbtool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+public class ADB_Main {
+
+	private static Display display;
+	private static Text tx1;
+	private static Button btn1;
+	private static Button btn2;
+	private static Button btn3;
+	private static Button btn4;
+	private static Button btn5;
+	private static Button btn6;
+
+	static String stremulator = null;
+
+	// private static String str_args = "";
+
+	static String FileOpem(Shell shell) {
+
+		FileDialog fd = new FileDialog(shell, SWT.OPEN);
+		fd.setText("Open");
+		fd.setFilterPath("./");
+		String[] filterExt = { "*.apk" };
+		fd.setFilterExtensions(filterExt);
+		String selected = fd.open();
+		System.out.println(selected);
+
+		return selected;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		display = new Display();
+		final Shell shell = new Shell(display);
+		shell.setText("ADB Test Tool");
+
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 3;
+		gridLayout.makeColumnsEqualWidth = true;
+
+		shell.setLayout(gridLayout);
+		shell.setSize(640, 480);
+
+		tx1 = new Text(shell, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		tx1.setText("This ADB Tool");
+		// tx1.setFont(SWTResourceManager.getFont("微軟正黑體", 10, 0));
+		GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		gridData.horizontalAlignment = SWT.CENTER;
+		gridData.verticalAlignment = SWT.CENTER;
+		gridData.horizontalSpan = 5;
+		gridData.widthHint = 600;
+		gridData.heightHint = 360;
+		tx1.setLayoutData(gridData);
+
+		GridData gridData2 = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		btn1 = new Button(shell, SWT.PUSH);
+		btn1.setText("ADB Devices");
+		btn1.setLayoutData(gridData2);
+		btn2 = new Button(shell, SWT.PUSH);
+		btn2.setText("ADB Install APP");
+		btn2.setLayoutData(gridData2);
+		btn3 = new Button(shell, SWT.PUSH);
+		btn3.setText("ADB Filetr APP");
+		btn3.setLayoutData(gridData2);
+		btn4 = new Button(shell, SWT.PUSH);
+		btn4.setText("ADB Select Device");
+		btn4.setLayoutData(gridData2);
+		btn5 = new Button(shell, SWT.PUSH);
+		btn5.setText("Null");
+		btn5.setLayoutData(gridData2);
+		btn6 = new Button(shell, SWT.PUSH);
+		btn6.setText("ADB Delete APP");
+		btn6.setLayoutData(gridData2);
+
+		btn1.addSelectionListener(new SelectionAdapter() {
+			@SuppressWarnings("unused")
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Button 1 Called!");
+				tx1.setText(btn1.getText() + " Called!");
+
+				String path = System.getProperty("user.dir")
+						+ "\\platform-tools\\";
+				System.out.println(path);
+				tx1.setText(btn1.getText() + "\n" + path);
+
+				// final String commandString = "cmd /c adb devices";
+				final String commandString = ".\\platform-tools\\adb.exe devices";
+				// final String commandString = "ping 8.8.8.8";
+
+				try {
+					String listString = null;
+
+					Process process = Runtime.getRuntime().exec(commandString);
+
+					InputStream istream = process.getInputStream();
+
+					BufferedReader iReader = new BufferedReader(
+							new InputStreamReader(istream));
+
+					String input = iReader.readLine();
+
+					for (int i = 0; input != null; i++, input = iReader
+							.readLine()) {
+						// 若需執行結果可將input傳出
+						System.out.println(input);
+						if (listString != null) {
+							listString = listString + "\n" + input;
+						} else {
+							listString = input;
+						}
+					}
+					if (listString != null) {
+						tx1.setText(listString);
+					} else {
+						tx1.setText("String Null... ");
+					}
+
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		btn2.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Button 2 Called!");
+				tx1.setText(btn2.getText() + " Called!");
+
+				String path = FileOpem(shell);
+				tx1.setText(tx1.getText() + "\n" + path);
+
+				final String commandString;
+
+				if (stremulator != null) {
+					commandString = ".\\platform-tools\\adb.exe " + stremulator
+							+ "install " + path;
+				} else {
+					commandString = ".\\platform-tools\\adb.exe " + "install "
+							+ path;
+				}
+
+				System.out.println(commandString);
+
+				try {
+					String listString = null;
+
+					Process process = Runtime.getRuntime().exec(commandString);
+
+					InputStream istream = process.getInputStream();
+
+					BufferedReader iReader = new BufferedReader(
+							new InputStreamReader(istream));
+
+					String input = iReader.readLine();
+
+					for (int i = 0; input != null; i++, input = iReader
+							.readLine()) {
+						// 若需執行結果可將input傳出
+						System.out.println(input);
+						if (listString != null) {
+							listString = listString + "\n" + input;
+						} else {
+							listString = input;
+						}
+					}
+					if (listString != null) {
+						tx1.setText(listString);
+					} else {
+						tx1.setText("String Null... ");
+					}
+
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
+		btn3.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Button 3 Called!");
+				// tx1.setText(btn3.getText() + " Called!");
+
+				String strfiler = tx1.getSelectionText();
+
+				System.out.println(strfiler);
+
+				final String commandString;
+
+				if (stremulator != null) {
+					commandString = ".\\platform-tools\\adb.exe " + stremulator
+							+ "shell pm list packages " + strfiler;
+				} else {
+					commandString = ".\\platform-tools\\adb.exe "
+							+ "shell pm list packages " + strfiler;
+				}
+
+				System.out.println(commandString);
+
+				try {
+					String listString = null;
+
+					Process process = Runtime.getRuntime().exec(commandString);
+
+					InputStream istream = process.getInputStream();
+
+					BufferedReader iReader = new BufferedReader(
+							new InputStreamReader(istream));
+
+					String input = iReader.readLine();
+
+					for (int i = 0; input != null; i++, input = iReader
+							.readLine()) {
+						// 若需執行結果可將input傳出
+						System.out.println(input);
+						if (listString != null) {
+							listString = listString + "\n" + input;
+						} else {
+							listString = input;
+						}
+					}
+					if (listString != null) {
+						tx1.setText(listString);
+					} else {
+						tx1.setText("String Null... ");
+					}
+
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		btn4.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Button 4 Called!");
+				// tx1.setText(btn4.getText() + " Called!");
+
+				stremulator = tx1.getSelectionText();
+
+				if (stremulator == "") {
+					tx1.setText("None Select Device!!");
+					stremulator = null;
+					return;
+				} else {
+					stremulator = "-s " + stremulator;
+				}
+
+				System.out.println(stremulator);
+				tx1.setText(stremulator + " is be Select!");
+
+			}
+		});
+
+		btn5.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Button 5 Called!");
+				tx1.setText(btn5.getText() + " Called!");
+			}
+		});
+
+		btn6.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Button 6 Called!");
+				// tx1.setText(btn6.getText() + " Called!");
+
+				String apkpackage = tx1.getSelectionText();
+
+				if (apkpackage == "") {
+					tx1.setText("None Select Apk Package!!");
+					return;
+				}
+
+				System.out.println(apkpackage);
+
+				final String commandString;
+
+				if (stremulator != null) {
+					commandString = ".\\platform-tools\\adb.exe " + stremulator
+							+ "uninstall " + apkpackage;
+				} else {
+					commandString = ".\\platform-tools\\adb.exe "
+							+ "uninstall " + apkpackage;
+				}
+
+				System.out.println(commandString);
+
+				try {
+					String listString = null;
+
+					Process process = Runtime.getRuntime().exec(commandString);
+
+					InputStream istream = process.getInputStream();
+
+					BufferedReader iReader = new BufferedReader(
+							new InputStreamReader(istream));
+
+					String input = iReader.readLine();
+
+					for (int i = 0; input != null; i++, input = iReader
+							.readLine()) {
+						// 若需執行結果可將input傳出
+						System.out.println(input);
+						if (listString != null) {
+							listString = listString + "\n" + input;
+						} else {
+							listString = input;
+						}
+					}
+					if (listString != null) {
+						tx1.setText(listString);
+					} else {
+						tx1.setText("String Null... ");
+					}
+
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		Control[] controls = new Control[] { btn1, btn2, btn3 };
+		shell.setTabList(controls);
+
+		// shell.pack(); // 佈局重新計算大小重設
+
+		shell.open();
+
+		// run the event loop as long as the window is open
+		while (!shell.isDisposed()) {
+			// read the next OS event queue and transfer it to a SWT event
+			if (!display.readAndDispatch()) {
+				// if there are currently no other OS event to process
+				// sleep until the next OS event is available
+				display.sleep();
+			}
+			// tx1.setText(getStr_arg1());
+		}
+		// disposes all associated windows and their components
+		display.dispose();
+	}
+}
---
+++ src/org/eclipse/wb/swt/SWTResourceManager.java
@@ -0,0 +1,274 @@
+package org.eclipse.wb.swt;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Display;
+
+public class SWTResourceManager {
+	private static Map<RGB, Color> m_colorMap = new HashMap();
+
+	private static Map<String, Image> m_imageMap = new HashMap();
+	private static final int MISSING_IMAGE_SIZE = 10;
+	public static final int TOP_LEFT = 1;
+	public static final int TOP_RIGHT = 2;
+	public static final int BOTTOM_LEFT = 3;
+	public static final int BOTTOM_RIGHT = 4;
+	protected static final int LAST_CORNER_KEY = 5;
+	private static Map<Image, Map<Image, Image>>[] m_decoratedImageMap = new Map[5];
+
+	private static Map<String, Font> m_fontMap = new HashMap();
+
+	private static Map<Font, Font> m_fontToBoldFontMap = new HashMap();
+
+	private static Map<Integer, Cursor> m_idToCursorMap = new HashMap();
+
+	public static Color getColor(int systemColorID) {
+		Display display = Display.getCurrent();
+		return display.getSystemColor(systemColorID);
+	}
+
+	public static Color getColor(int r, int g, int b) {
+		return getColor(new RGB(r, g, b));
+	}
+
+	public static Color getColor(RGB rgb) {
+		Color color = (Color) m_colorMap.get(rgb);
+		if (color == null) {
+			Display display = Display.getCurrent();
+			color = new Color(display, rgb);
+			m_colorMap.put(rgb, color);
+		}
+		return color;
+	}
+
+	public static void disposeColors() {
+		for (Color color : m_colorMap.values()) {
+			color.dispose();
+		}
+		m_colorMap.clear();
+	}
+
+	protected static Image getImage(InputStream stream) throws IOException {
+		try {
+			Display display = Display.getCurrent();
+			ImageData data = new ImageData(stream);
+			Image localImage;
+			if (data.transparentPixel > 0) {
+				return new Image(display, data, data.getTransparencyMask());
+			}
+			return new Image(display, data);
+		} finally {
+			stream.close();
+		}
+	}
+
+	public static Image getImage(String path) {
+		Image image = (Image) m_imageMap.get(path);
+		if (image == null) {
+			try {
+				image = getImage(new FileInputStream(path));
+				m_imageMap.put(path, image);
+			} catch (Exception e) {
+				image = getMissingImage();
+				m_imageMap.put(path, image);
+			}
+		}
+		return image;
+	}
+
+	public static Image getImage(Class<?> clazz, String path) {
+		String key = clazz.getName() + '|' + path;
+		Image image = (Image) m_imageMap.get(key);
+		if (image == null) {
+			try {
+				image = getImage(clazz.getResourceAsStream(path));
+				m_imageMap.put(key, image);
+			} catch (Exception e) {
+				image = getMissingImage();
+				m_imageMap.put(key, image);
+			}
+		}
+		return image;
+	}
+
+	private static Image getMissingImage() {
+		Image image = new Image(Display.getCurrent(), 10, 10);
+
+		GC gc = new GC(image);
+		gc.setBackground(getColor(3));
+		gc.fillRectangle(0, 0, 10, 10);
+		gc.dispose();
+
+		return image;
+	}
+
+	public static Image decorateImage(Image baseImage, Image decorator) {
+		return decorateImage(baseImage, decorator, 4);
+	}
+
+	public static Image decorateImage(Image baseImage, Image decorator,
+			int corner) {
+		if ((corner <= 0) || (corner >= 5)) {
+			throw new IllegalArgumentException("Wrong decorate corner");
+		}
+		Map cornerDecoratedImageMap = m_decoratedImageMap[corner];
+		if (cornerDecoratedImageMap == null) {
+			cornerDecoratedImageMap = new HashMap();
+			m_decoratedImageMap[corner] = cornerDecoratedImageMap;
+		}
+		Map decoratedMap = (Map) cornerDecoratedImageMap.get(baseImage);
+		if (decoratedMap == null) {
+			decoratedMap = new HashMap();
+			cornerDecoratedImageMap.put(baseImage, decoratedMap);
+		}
+
+		Image result = (Image) decoratedMap.get(decorator);
+		if (result == null) {
+			Rectangle bib = baseImage.getBounds();
+			Rectangle dib = decorator.getBounds();
+
+			result = new Image(Display.getCurrent(), bib.width, bib.height);
+
+			GC gc = new GC(result);
+			gc.drawImage(baseImage, 0, 0);
+			if (corner == 1)
+				gc.drawImage(decorator, 0, 0);
+			else if (corner == 2)
+				gc.drawImage(decorator, bib.width - dib.width, 0);
+			else if (corner == 3)
+				gc.drawImage(decorator, 0, bib.height - dib.height);
+			else if (corner == 4) {
+				gc.drawImage(decorator, bib.width - dib.width, bib.height
+						- dib.height);
+			}
+			gc.dispose();
+
+			decoratedMap.put(decorator, result);
+		}
+		return result;
+	}
+
+	public static void disposeImages() {
+		for (Image image : m_imageMap.values()) {
+			image.dispose();
+		}
+		m_imageMap.clear();
+
+		for (int i = 0; i < m_decoratedImageMap.length; i++) {
+			Map cornerDecoratedImageMap = m_decoratedImageMap[i];
+			if (cornerDecoratedImageMap != null) {
+				for (Map decoratedMap : cornerDecoratedImageMap.values()) {
+					for (Image image : decoratedMap.values()) {
+						image.dispose();
+					}
+					decoratedMap.clear();
+				}
+				cornerDecoratedImageMap.clear();
+			}
+		}
+	}
+
+	public static Font getFont(String name, int height, int style) {
+		return getFont(name, height, style, false, false);
+	}
+
+	public static Font getFont(String name, int size, int style,
+			boolean strikeout, boolean underline) {
+		String fontName = name + '|' + size + '|' + style + '|' + strikeout
+				+ '|' + underline;
+		Font font = (Font) m_fontMap.get(fontName);
+		if (font == null) {
+			FontData fontData = new FontData(name, size, style);
+			if ((strikeout) || (underline)) {
+				try {
+					Class logFontClass = Class
+							.forName("org.eclipse.swt.internal.win32.LOGFONT");
+					Object logFont = FontData.class.getField("data").get(
+							fontData);
+					if ((logFont != null) && (logFontClass != null)) {
+						if (strikeout) {
+							logFontClass.getField("lfStrikeOut").set(logFont,
+									Byte.valueOf((byte) 1));
+						}
+						if (underline)
+							logFontClass.getField("lfUnderline").set(logFont,
+									Byte.valueOf((byte) 1));
+					}
+				} catch (Throwable e) {
+					System.err
+							.println("Unable to set underline or strikeout (probably on a non-Windows platform). "
+									+ e);
+				}
+			}
+			font = new Font(Display.getCurrent(), fontData);
+			m_fontMap.put(fontName, font);
+		}
+		return font;
+	}
+
+	public static Font getBoldFont(Font baseFont) {
+		Font font = (Font) m_fontToBoldFontMap.get(baseFont);
+		if (font == null) {
+			FontData[] fontDatas = baseFont.getFontData();
+			FontData data = fontDatas[0];
+			font = new Font(Display.getCurrent(), data.getName(),
+					data.getHeight(), 1);
+			m_fontToBoldFontMap.put(baseFont, font);
+		}
+		return font;
+	}
+
+	public static void disposeFonts() {
+		for (Font font : m_fontMap.values()) {
+			font.dispose();
+		}
+		m_fontMap.clear();
+
+		for (Font font : m_fontToBoldFontMap.values()) {
+			font.dispose();
+		}
+		m_fontToBoldFontMap.clear();
+	}
+
+	public static Cursor getCursor(int id) {
+		Integer key = Integer.valueOf(id);
+		Cursor cursor = (Cursor) m_idToCursorMap.get(key);
+		if (cursor == null) {
+			cursor = new Cursor(Display.getDefault(), id);
+			m_idToCursorMap.put(key, cursor);
+		}
+		return cursor;
+	}
+
+	public static void disposeCursors() {
+		for (Cursor cursor : m_idToCursorMap.values()) {
+			cursor.dispose();
+		}
+		m_idToCursorMap.clear();
+	}
+
+	public static void dispose() {
+		disposeColors();
+		disposeImages();
+		disposeFonts();
+		disposeCursors();
+	}
+}
+
+/*
+ * Location: F:\Jackey\tools_ch\TestingTools.jar Qualified Name:
+ * org.eclipse.wb.swt.SWTResourceManager JD-Core Version: 0.6.2
+ */
